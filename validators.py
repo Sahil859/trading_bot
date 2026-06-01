d7c21a6d620768b964e4904eddb7dbd2723f652b
Sahil khan <khansahil20216@gmail.com>
2026-06-01 22:37:31 +0530

    Add files via upload

---
+++ validators.py
@@ -0,0 +1,31 @@
+def validate_side(side):
+    side = side.upper()
+
+    if side not in ["BUY", "SELL"]:
+        raise ValueError("Side must be BUY or SELL")
+
+    return side
+
+
+def validate_order_type(order_type):
+    order_type = order_type.upper()
+
+    if order_type not in ["MARKET", "LIMIT"]:
+        raise ValueError("Order type must be MARKET or LIMIT")
+
+    return order_type
+
+
+def validate_quantity(quantity):
+    if quantity <= 0:
+        raise ValueError("Quantity must be greater than 0")
+
+    return quantity
+
+
+def validate_price(price, order_type):
+    if order_type == "LIMIT":
+        if price is None or price <= 0:
+            raise ValueError("Valid price required for LIMIT order")
+
+    return price
